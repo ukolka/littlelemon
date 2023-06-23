774c2a32994dc4ecb6c92ad7539b427a77981774
Mykola Bespaliuk <mykola@adbeat.com>
2023-06-23 12:45:31 -0400

    added models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.title
